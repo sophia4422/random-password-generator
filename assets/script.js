df103429cb2bc9a5de40e202ed3c72b76f50a874
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-04-23 22:03:08 +0100

    password length prompt

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,31 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
+// Prompt the user to give a desired password length
 const getPasswordLength = () => {
-  return 10;
+  //prompt user to enter a length with a pop-up box
+  const whatLength = window.prompt(
+    "How long would you like your password to be?"
+  );
+
+  //turn the users answer, which is a string, into a number
+  const passwordLength = parseInt(whatLength);
+
+  //Need a function to check that the input is equal to or more than 8 or equal to or less than 128 characters (if/else statement)
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    return passwordLength;
+  } else {
+    window.prompt("Password must be at least 8 and under 128 characters long");
+  }
 };
+
+// Prompt the user to select what criteria they want the password to contain,
+// Lowercase? Uppercase? Numbers? Symbols? They must pick at least one.
+
+//Need a const for characters //    "abcdefghijklmnopqrstuvwxyz",
+// "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+//  "0123456789",
+// " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~",
 
 const getPasswordCriteria = () => {
   return [
@@ -14,6 +36,7 @@ const getPasswordCriteria = () => {
   ];
 };
 
+// This will take in passwordLength and passwordCriteria and return a string
 const createRandomPassword = () => {
   return "kdUE28(@d0";
 };
